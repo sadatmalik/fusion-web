3032c27b0e741afb1d59087dc512329ddd3d40fe
sadat.malik <sadat.malik@me.com>
2022-01-12 20:19:03 +0000

    Spring.io initial project files

---
+++ src/main/java/com/sadatmalik/fusionweb/FusionWebPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.sadatmalik.fusionweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionWebPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusionWebPrototypeApplication.class, args);
+	}
+
+}
