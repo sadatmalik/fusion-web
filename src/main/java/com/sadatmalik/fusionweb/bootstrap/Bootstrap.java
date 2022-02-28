5a5e6daba281270f1f949a71cba81c2bfc853a18
sadat.malik <sadat.malik@me.com>
2022-02-28 20:06:45 +0000

    bootstrap QA profile

--- src/main/java/com/sadatmalik/fusionweb/bootstrap/Bootstrap.java
+++ src/main/java/com/sadatmalik/fusionweb/bootstrap/Bootstrap.java
@@ -24,7 +24,7 @@ import java.util.Collections;
 import java.util.List;
 
 @Slf4j
-@Profile("dev")
+@Profile({"dev", "qa"})
 @RequiredArgsConstructor
 @Component
 public class Bootstrap implements CommandLineRunner {
