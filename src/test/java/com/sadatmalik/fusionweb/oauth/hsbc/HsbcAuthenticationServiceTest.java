4980b3c83efa8f1b8ad8e718dfcfd78cd5eeb23a
sadat.malik <sadat.malik@me.com>
2022-03-14 14:37:45 +0000

    reenable failing test 2 for deploy debug

--- src/test/java/com/sadatmalik/fusionweb/oauth/hsbc/HsbcAuthenticationServiceTest.java
+++ src/test/java/com/sadatmalik/fusionweb/oauth/hsbc/HsbcAuthenticationServiceTest.java
@@ -1,7 +1,6 @@
 package com.sadatmalik.fusionweb.oauth.hsbc;
 
 import com.nimbusds.jose.JWSObject;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,7 +31,6 @@ class HsbcAuthenticationServiceTest {
     }
 
     @Test
-    //@Disabled // just until deployment
     void testCreateNimbusJwt() {
         HsbcConsent consent = new HsbcConsent();
         consent.data = new HsbcConsentData();
@@ -45,7 +43,7 @@ class HsbcAuthenticationServiceTest {
     }
 
     @Test
-    @Disabled // just until deployment
+    //@Disabled // just until deployment
     void testGetAuthorizationURL() {
         accessToken = service.getAccessToken();
         consent = service.getConsentID(accessToken);
