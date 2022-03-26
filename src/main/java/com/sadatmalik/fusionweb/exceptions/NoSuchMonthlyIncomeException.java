323ce68c26daff1a96dae3df42ef1cb964425f30
sadat.malik <sadat.malik@me.com>
2022-03-26 16:05:37 +0000

    java doc for exceptions package. incremental #141

--- src/main/java/com/sadatmalik/fusionweb/exceptions/NoSuchMonthlyIncomeException.java
+++ src/main/java/com/sadatmalik/fusionweb/exceptions/NoSuchMonthlyIncomeException.java
@@ -1,7 +1,20 @@
 package com.sadatmalik.fusionweb.exceptions;
 
+/**
+ * Exception intended for use when the downstream lookup of monthly income
+ * data does not return any values.
+ *
+ * @author sadatmalik
+ */
 public class NoSuchMonthlyIncomeException extends Throwable {
 
+    /**
+     * Intended usage is to always provide an informational message when the
+     * exception is constructed. Thus, only this parameterised constructor
+     * is exposed.
+     *
+     * @param message exception message.
+     */
     public NoSuchMonthlyIncomeException(String message) {
         super(message);
     }
