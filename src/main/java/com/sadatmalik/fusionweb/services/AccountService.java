829e55b9eba8c7a1d629150b4996bedbc10a865f
sadat.malik <sadat.malik@me.com>
2022-04-18 16:09:37 +0100

    java doc for account service. incremental #141

--- src/main/java/com/sadatmalik/fusionweb/services/AccountService.java
+++ src/main/java/com/sadatmalik/fusionweb/services/AccountService.java
@@ -9,6 +9,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * The service encapsulates  methods that clients of the service can call for
+ * Crud and other account related common behaviours and requests.
+ *
+ * Uses an instance of AccountRepository for DB interactions.
+ *
+ * @author sadatmalik
+ */
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -16,14 +24,33 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
 
+    /**
+     * Get a list of all accounts for the given user.
+     *
+     * @param user the user.
+     * @return a list of accounts for the given user.
+     */
     public List<Account> getAccountsFor(User user) {
         return accountRepository.findByUser(user);
     }
 
+    /**
+     * Get and return the single account with the given accound id.
+     *
+     * @param accountId the account id to query.
+     * @return the account with the given account id.
+     */
     public Account getAccountById(String accountId) {
         return accountRepository.findByAccountId(accountId);
     }
 
+    /**
+     * Save the given account details for the given user.
+     *
+     * @param user the user owning the account.
+     * @param account the account.
+     * @return the saved account.
+     */
     public Account saveUserAccount(User user, Account account) {
         account.setUser(user);
         return accountRepository.save(account);
