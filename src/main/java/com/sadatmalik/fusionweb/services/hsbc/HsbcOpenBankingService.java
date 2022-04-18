0972c1014dbee380977f2fb6c8a580e628bbc7dc
sadat.malik <sadat.malik@me.com>
2022-04-18 14:48:07 +0100

    java doc for Hsbc OB service interface. incremental #141

--- src/main/java/com/sadatmalik/fusionweb/services/hsbc/HsbcOpenBankingService.java
+++ src/main/java/com/sadatmalik/fusionweb/services/hsbc/HsbcOpenBankingService.java
@@ -2,6 +2,15 @@ package com.sadatmalik.fusionweb.services.hsbc;
 
 import com.sadatmalik.fusionweb.services.OpenBankingService;
 
+/**
+ * An interface intended to encapsulate the required methods that any service
+ * interacting with the Hsbc OB Api must expose to clients.
+ *
+ * The interface also serves as the central repository for all Api related Urls,
+ * which it maintains as String constants.
+ *
+ * @author sadatmalik
+ */
 public interface HsbcOpenBankingService extends OpenBankingService {
     String ACCOUNT_INFO_URL = "https://sandbox.hsbc.com/psd2/obie/v3.1/accounts";
 }
