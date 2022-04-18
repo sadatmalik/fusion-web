7294fa34d5fa8f022ef9ecb96a6f6c0c068907b7
sadat.malik <sadat.malik@me.com>
2022-04-18 14:59:36 +0100

    java doc for custom user details service. incremental #141

--- src/main/java/com/sadatmalik/fusionweb/services/websecurity/CustomUserDetailsService.java
+++ src/main/java/com/sadatmalik/fusionweb/services/websecurity/CustomUserDetailsService.java
@@ -7,12 +7,30 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+/**
+ * Implements the UserDetailsService interface, specifically the loadUserByUsername(..)
+ * method which interacts with the user database tables via the injected userPrincipalRepo.
+ *
+ * This service is used by the WebSecurity configuration to load user information for the
+ * user login authentication process.
+ *
+ * @see com.sadatmalik.fusionweb.config.WebSecurityConfig
+ * @author sadatmalik
+ */
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     UserPrincipalRepo userPrincipalRepo;
 
+    /**
+     * Loads a user principal details from the user principal repo, querying by the
+     * given username.
+     *
+     * @param username from the current login.
+     * @return the user principal details from the user database.
+     * @throws UsernameNotFoundException
+     */
     @Override
     public UserPrincipal loadUserByUsername(String username) throws UsernameNotFoundException {
         return userPrincipalRepo.findByUsername(username)
