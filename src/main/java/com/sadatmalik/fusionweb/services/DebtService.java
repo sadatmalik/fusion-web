79c770b994f1ef70d421cf4182eeaf3c647d116c
sadat.malik <sadat.malik@me.com>
2022-04-18 16:16:49 +0100

    java doc for debt service. incremental #141

--- src/main/java/com/sadatmalik/fusionweb/services/DebtService.java
+++ src/main/java/com/sadatmalik/fusionweb/services/DebtService.java
@@ -9,6 +9,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * A service that encapsulates all methods that a client of the service may call
+ * for Crud and other debt related common behaviours and requests.
+ *
+ * Uses an instance of DebtRepository for database interactions.
+ *
+ * @author sadatmalik
+ */
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -16,6 +24,12 @@ public class DebtService {
 
     private final DebtRepository debtRepository;
 
+    /**
+     * Queries the database and returns all stored debt for the given user.
+     *
+     * @param user the user.
+     * @return a list of debt objects for the given user.
+     */
     public List<Debt> getDebtFor(User user) {
         return debtRepository.findAllByUser(user);
     }
